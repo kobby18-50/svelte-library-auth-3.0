3e42755417d17354052afc60bed5b88936068dbe
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2023-11-03 13:34:50 +0000

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script>
+    import {page} from '$app/stores'
+	import { Heading } from 'flowbite-svelte';
+    import SERVER_DOWN from '../assets/server_err.svg'
+
+    console.log($page)
+
+</script>
+
+<main>
+    {#if $page.url.pathname !== '/'}
+
+    <div class="text-center text-lg">
+        <p>{$page.error?.message}</p>
+    </div>
+    
+    
+       <figure class=" flex items-center justify-center flex-col my-36">
+        <img src={SERVER_DOWN} alt="">
+
+        <figcaption class="mt-20 text-center">
+            It seems something went wrong
+            <br>
+
+            <a href="/" class='text-primary-700'>Go back to homepage</a>
+        </figcaption>
+       </figure>
+
+   
+        
+    {/if}
+</main>
