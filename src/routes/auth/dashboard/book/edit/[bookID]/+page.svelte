b6666a8796a3c8ab7a2cd179d0dec8ee96610580
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2023-11-17 12:54:09 +0000

    edit book validation

--- src/routes/auth/dashboard/book/edit/[bookID]/+page.svelte
+++ src/routes/auth/dashboard/book/edit/[bookID]/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { BASE_URL, Categories } from "$lib";
 	import axios from "axios";
-	import { onMount } from "svelte";
-    import { page } from "$app/stores";
 	import { Alert, Button, Heading, Input, Label, Select, Spinner, Textarea } from "flowbite-svelte";
 	import { goto } from "$app/navigation";
-	import { InfoCircleSolid } from "flowbite-svelte-icons";
 	import toast, {Toaster} from "svelte-french-toast";
     import {user_token} from '$lib/stores/tokenStore.js'
+    import { createForm } from 'felte'
+    import { validator } from '@felte/validator-yup'
+    import * as yup from 'yup'
+    import { reporter, ValidationMessage } from '@felte/reporter-svelte'
 
    
 
@@ -15,41 +16,41 @@
 
    let book = data.book
 
-   let isFail = false
-
-   let editValidator = {
-        titleValidator : '',
-        contentValidator : ''
-    }
-
-// token from store
-    const token = $user_token
+   let isLoading = false
 
 
+    // yup schema
+    const schema = yup.object().shape({
+        title : yup.string().min(8, 'Title must be at least 8 characters long').required('Title is required'),
+        content : yup.string().min(40, 'Content must be at least 40 characters long').required('Content is required'),
+        genre : yup.string().required('Genre is required')
 
-    let isLoading = false
-
-   
-
-
-   
-const handleEdit = async () => {
-    isLoading = true
-    if(book.title.length <= 8){
-            editValidator.titleValidator = 'The minimum length of book title is 8 characters'
-        }
-        if(book.content.length <= 40){
-            editValidator.contentValidator = 'The minimum length of book content is 40 characters'
-        }
-    const data = {
-        title : book.title,
-        content : book.content,
-        genre : book.genre
-    }
+    })
 
 
-    await axios.patch(`${BASE_URL}/books/${book._id}`, data, {headers : {Authorization : `Bearer ${token}`}})
-    .then((res) => {
+    const { form, isValid } = createForm({
+        initialValues : {
+            title : book.title,
+            genre : book.genre,
+            content : book.content
+        },
+
+        onSubmit(values, context) {
+            const { title, genre, content} = values
+
+            const data = {
+                title,
+                genre,
+                content
+            }
+
+            // token from store
+            const token = $user_token
+
+            isLoading = true
+
+            axios.patch(`${BASE_URL}/books/${book._id}`, data, {headers : {Authorization : `Bearer ${token}`}})
+        .then((res) => {
         if(res.status === 200){
             isLoading = false
             toast.success('Book updated successfully')
@@ -59,8 +60,9 @@ const handleEdit = async () => {
         }
     })
     .catch((err) => {
-        isFail = true
         isLoading = false
+
+        console.log(err)
       
         if(err.request.response.includes('Duplicate')){
                 toast.error('This title has already been used try a different one')
@@ -69,7 +71,12 @@ const handleEdit = async () => {
 
             }
     })
-}
+        },
+
+        extend : [
+            validator({schema}), reporter
+        ]
+    })
 
 </script>
 
@@ -81,42 +88,49 @@ const handleEdit = async () => {
 
     <Heading tag='h6' class='text-center mb-5'>Edit Book</Heading>
 
-    <form on:submit|preventDefault={handleEdit} class="mx-10 grid grid-row-2 gap-5 md:mx-24 lg:mx-52">
+    <form use:form class="mx-10 grid grid-row-2 gap-5 md:mx-24 lg:mx-52">
 
         <div>
             <Label for='title'>Book title</Label>
-            <Input id='title' placeholder='Grief Child' bind:value={book.title} required />
-
-            {#if isFail && editValidator.titleValidator.length != 0}
-            <Alert color="red" dismissable class='mt-2'>
-                <InfoCircleSolid slot="icon" class="w-4 h-4" />
-                 {editValidator.titleValidator}
-                <Button slot="close-button" size="xs" let:close on:click={close} class="ml-auto">X</Button>
-              </Alert>
-            {/if}
+            <Input id='title' placeholder='Grief Child' name='title'  required />
+            <ValidationMessage for ='title' let:messages={message}>
+                {#if message}
+                <Alert class='mt-2'>
+                    <span>{message || ''}</span>
+                </Alert>
+                {/if}
+            </ValidationMessage>
         </div>
 
         <div>
-            <Label for='title'>Book Genre</Label>
-           <Select bind:value={book.genre} required >
+            <Label for='genre'>Book Genre</Label>
+           <Select name ='genre' required >
             {#each Categories as {genre, id} (id) }
                 <option value={genre}>{genre}</option>
             {/each}
            </Select>
+           <ValidationMessage for ='genre' let:messages={message}>
+            {#if message}
+            <Alert class='mt-2'>
+                <span>{message || ''}</span>
+            </Alert>
+            {/if}
+        </ValidationMessage>
         </div>
 
         
         <div>
             <Label for='content'>Book Content</Label>
-            <Textarea id='content' placeholder='Book content' rows='5' bind:value={book.content} required />
+            <Textarea id='content' placeholder='Book content' rows='5' name='content' required />
+            <ValidationMessage for ='content' let:messages={message}>
+                {#if message}
+                <Alert class='mt-2'>
+                    <span>{message || ''}</span>
+                </Alert>
+                {/if}
+            </ValidationMessage>
 
-            {#if isFail && editValidator.contentValidator.length != 0}
-            <Alert color="red" dismissable class='mt-2'>
-                <InfoCircleSolid slot="icon" class="w-4 h-4" />
-                 {editValidator.contentValidator}
-                <Button slot="close-button" size="xs" let:close on:click={close} class="ml-auto">X</Button>
-              </Alert>
-            {/if}
+           
             
         </div>
 
@@ -127,7 +141,7 @@ const handleEdit = async () => {
         </Button>
         {:else}
         
-        <Button type='submit'>Edit Book</Button>
+        <Button disabled = {$isValid ? false : true} type='submit'>Edit Book</Button>
         
         
         
